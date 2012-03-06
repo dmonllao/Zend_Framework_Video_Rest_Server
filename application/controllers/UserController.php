130d23a9455d440eccec817ef7d4ed5176091f4b
David Monllaó <david.monllao@gmail.com>
2012-03-06 15:59:48 +0100

    Adding user/get action

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -10,9 +10,16 @@ class UserController extends Zend_Controller_Action
 
     public function indexAction()
     {
+        $this->view->action = 'index de user';
+    }
+
+    public function getAction()
+    {
         // action body
     }
 
 
 }
+
+
 
