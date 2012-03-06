20a15d11cf56e2166b53a29cafc5419b197ffa30
David Monllaó <david.monllao@gmail.com>
2012-03-06 18:41:46 +0100

    Adding a controller helper to switch to context of REST actions to the requested format

---
+++ library/App/Controller/Helper/RestContexts.php
@@ -0,0 +1,41 @@
+<?php
+
+class App_Controller_Helper_RestContexts extends Zend_Controller_Action_Helper_Abstract 
+{
+
+    protected $_contexts = array(
+        'xml', 
+        'json',
+    );
+
+    public function preDispatch()
+    {
+        // If it is a controlled that supports REST methods 
+        // (inherited from App_Rest_Controller) overwrite the context
+        $controller = $this->getActionController();
+        if (!$controller instanceof App_Rest_Controller) {
+            return;
+        }
+
+        $this->_initContexts();
+
+        // Set a Vary response header based on the Accept header
+        $this->getResponse()->setHeader('Vary', 'Accept');
+    }
+
+    /**
+     * Sets everyone of the REST methods to respond in the appropiate format
+     */ 
+    protected function _initContexts()
+    {
+        $cs = $this->getActionController()->contextSwitch;
+        $cs->setAutoJsonSerialization(false);
+        foreach ($this->_contexts as $context) {
+            foreach (array('index', 'post', 'get', 'put', 'delete') as $action) {
+                $cs->addActionContext($action, $context);
+            }
+        }
+        $cs->initContext();
+    }
+}
+
