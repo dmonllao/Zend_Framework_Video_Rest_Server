02338a1d121180c03fa2e822a106101b4ff2440c
David Monllaó <david.monllao@gmail.com>
2012-04-28 21:24:59 +0200

    Db table for videos

---
+++ application/models/DbTable/Video.php
@@ -0,0 +1,15 @@
+<?php
+
+class Application_Model_DbTable_Video extends Application_Model_DbTable_General
+{
+
+    protected $_name = 'videos';
+
+    /**
+     * To specify the set of available fields
+     * @var array
+     */
+    protected $_fields = array('id', 'userid', 'name', 'filename', 'timecreated');
+
+}
+
